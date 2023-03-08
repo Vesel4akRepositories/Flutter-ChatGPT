cbc02065d82171476605b062b18d7e4886a4d718
Vesel4ak <68870977+Vesel4akRepositories@users.noreply.github.com>
2023-03-09 01:33:03 +0300

    Update choices

--- lib/src/model/complete_text/response/choices.dart
+++ lib/src/model/complete_text/response/choices.dart
@@ -2,15 +2,20 @@ class Choices {
   final String text;
   final int index;
   final dynamic logprobs;
-  final String finish_reason;
+  final String? finish_reason;
 
-  Choices(this.text, this.index, this.logprobs, this.finish_reason);
+  Choices(
+    this.text,
+    this.index,
+    this.logprobs,
+    this.finish_reason,
+  );
 
   factory Choices.fromJson(Map<String, dynamic> json) => Choices(
         json['text'] as String,
         json['index'] as int,
         json['logprobs'],
-        json['finish_reason'] as String,
+        json['finish_reason'] as String?,
       );
 
   Map<String, dynamic> toJson() => _ChoicesToJson(this);
